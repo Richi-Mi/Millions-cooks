a5091d25e4f033e17b3abfb0fd72af53329f1f65
Jose Ricardo Mendoza Castañeda <mjoseanakin@gmail.com>
2020-06-11 11:15:35 -0500

    No hice casi nd

---
+++ JS/clases.js
@@ -0,0 +1,10 @@
+const elementos_principal = document.querySelector("reces");
+
+for(let i = 0; i <= 9; i++){
+    if(i % 2 == 0){
+        console.log('hola');
+    }
+    else{
+        console.log('Mundo');
+    }
+}
